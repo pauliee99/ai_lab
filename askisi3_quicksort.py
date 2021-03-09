72bdfd37f4bbf92624a1672be3afd14317e47f28
Pavlos Andreou <pavlosan67@gmail.com>
2021-03-09 23:09:48 +0200

    commit quicksort

--- askisi3_quicksort.py
+++ askisi3_quicksort.py
@@ -1,17 +1,10 @@
-def quicksort(l):
-    if len(l) == 0:
-        return
-
-    pivot = l[-1]
-    partition_1 = []
-    partition_2 = []
-    for i in range(len(l)):
-        if l[i] < pivot:
-            partition_1.append(l[i])
-        else:
-            partition_2.append(l[i])
-    quicksort(partition_1)
-    quicksort(partition_2)
+def quicksort(list):
+    if list == []: 
+        return []
+    pivot = list[0]
+    partition_1 = quicksort([x for x in list[1:] if x < pivot])
+    partition_2 = quicksort([x for x in list[1:] if x >= pivot])
+    return partition_1 + [pivot] + partition_2
 
 l= [4,12,5,8,13]
-quicksort(l)
+print(quicksort(l))
